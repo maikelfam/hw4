89e259fa3a4cb15b5b4d4d09691f4f8b22e9c768
maikelfam <maikel.fam@kellogg.northwestern.edu>
2022-03-13 22:09:14 +0000

    got buttons on home page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,26 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({ email: params["email"] })
+    if @user
+      if BCrypt::Password.new(@user.password) == params["password"]
+        session[:user_id] = @user.id
+        flash[:notice] = "Welcome, #{@user.username}."
+        redirect_to "/placesd"
+      else
+        flash[:notice] = "Incorrect Password"
+        redirect_to "/sessions/new"
+      end
+    else
+      flash[:notice] = "Email has not been registered"
+      redirect_to "/sessions/new"
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:notice] = "You have logged out"
+    redirect_to "/sessions/new"
   end
 end
   
